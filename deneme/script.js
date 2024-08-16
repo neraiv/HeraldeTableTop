de31a13edd7486ebd13c8e1258c42cc9e4cceb01
Yigit Sirin <122676073+neraiv@users.noreply.github.com>
2024-08-16 07:51:32 +0300

    Remeoved old content

--- deneme/script.js
+++
@@ -1,7 +0,0 @@
-function openScreen() {
-    document.getElementById('sliding-screen').style.left = '0'; // Slide in the screen
-}
-
-function closeScreen() {
-    document.getElementById('sliding-screen').style.left = '-100%'; // Slide out the screen
-}
--- release/javascript/icons.js
+++
@@ -1,7 +0,0 @@
-const iconsFolder = "images/menu-icons/";
-
-const icon_downArrowGreen = "downArrowGreen.png";
-const icon_newFile = "newFile.svg";
-const icon_characterSheet = "sheet-icon-7.png";
-const icon_rainbowDice = "rainbow-dice.png";
-const icon_closeBar = "close-bar.png";
